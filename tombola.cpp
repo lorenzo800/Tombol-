6eb1f8a007f4892ca9dd340ac12443e219c7ff63
lorenzo800 <previtali.lorenzo.studente@itispaleocapa.it>
2025-12-19 11:20:34 +0100

    Add files via upload

---
+++ tombola.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	
+	int x;
+	srand(time(NULL));
+	x=rand()%90+1; 
+	
+	cout<<x<<endl;
+
+	return 0;
+}
