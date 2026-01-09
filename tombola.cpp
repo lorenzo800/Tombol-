392bc2c77bf6a921947bebde7f6ea6377632c511
lorenzo800 <previtali.lorenzo.studente@itispaleocapa.it>
2026-01-09 11:53:24 +0100

    Update tombola.cpp

--- tombola.cpp
+++ tombola.cpp
@@ -1,31 +1,46 @@
-#include <iostream>
-#include <time.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
 using namespace std;
+int x;
+int main (){
 
-int main(int argc, char** argv) {
-	
-	
-	int x;
-	bool t[91] = {false}; 
+bool t[91] = {false}; 
 
-	srand(time(NULL));
-	x=rand()%90+1; 
-	
-	cout<<"Gioco della tombola"<<endl;
-	cout<<"Tabella iniziale:"<<endl;
-	
-	for(int i=1; i<91; i++){
-	if (t[i]) cout << "[X] ";
-    else cout <<i << " ";
-    
-    if (i % 10 == 0) cout << endl; 
+srand(time(NULL));
+x=rand()%90+1; 
+
+cout<<"Gioco della tombola"<<endl;
+cout<<"Tabella iniziale:"<<endl;
+
+for(int i=1; i<91; i++){
+if (t[i]) cout << "[X] ";
+
+else if(i<10){
+	cout<< i << "   ";
 }
-		
+else cout <<i << "  "; 
 
-	
-	
-	
 
-	return 0;
+if (i % 10 == 0) cout << endl;
+Sleep(40);
+}
+
+cout<<"Cartella giocatore 1";
+
+
+
+
+
+
+cout<<"Cartella giocatore 2";
+
+
+
+
+
+
+
+ return 0;
+
+
 }
